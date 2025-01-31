59636e54b81df33390c9fc736386629b647965e5
Tejas <tbhosale635@gmail.com>
2025-01-31 17:58:22 +0530

    Import Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Login from './Login'
-import SignUp from './SignUp'
-import Home from './Home'
-import About from './About'
-import Forget from './Forget'
-import Change from './Change'
-import Doubt from './Doubt'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import Home from "./Home";
+import About from "./About";
+import Forget from "./Forget";
+import Change from "./Change";
+import Doubt from "./Doubt";
 
 function App() {
-
   return (
     <BrowserRouter>
-        <Routes>
-          <Route path = '/' element = {<Login/>}/>
-          <Route path = '/signup' element = {<SignUp/>}/>
-          <Route path = '/home' element = {<Home/>}/>
-          <Route path = '/about' element = {<About/>}/>
-          <Route path = '/forget' element = {<Forget/>}/>
-          <Route path = '/change' element = {<Change/>}/>
-          <Route path = '/doubt' element = {<Doubt/>}/>
-
-
-
-
-
-
-
-        </Routes>
-    
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/forget" element={<Forget />} />
+        <Route path="/change" element={<Change />} />
+        <Route path="/doubt" element={<Doubt />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
